04b28f44ab56eabf5181aa4f46acb66559e98b71
Chaunceyan <Chauncey.ycx@gmail.com>
2014-12-03 01:28:19 +0100

    Finished With The Home Page.

--- where_are_we/routes/index.js
+++ where_are_we/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', name:'Chauncey' });
+});
+router.get('/test', function(req, res) {
+	res.render('index', { title: 'Where Are We', name:'Chauncey'});
 });
 
 module.exports = router;
